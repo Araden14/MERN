5eaf70e556e8d3aa6c7b4b7ad405b591b1360936
Araden14 <31803803+Araden14@users.noreply.github.com>
2023-08-05 14:08:15 +0200

    Update index.js URL

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors({
-  origin: 'https://mern-six-sooty.vercel.app', // Corrected URL
+  origin: 'https://mern-git-production-araden14.vercel.app', // Corrected URL
   credentials: true
 }));
 app.use(CookieParser());
