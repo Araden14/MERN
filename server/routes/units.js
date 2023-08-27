4eb55913057922630f9b5f9f5f81c002c3be6562
Araden14 <arnaud-danselme@hotmail.com>
2023-08-27 20:47:35 +0200

    Delete units feature (still buggy)

--- server/routes/units.js
+++ server/routes/units.js
@@ -5,7 +5,7 @@ import userVerification from "../middleware/AuthMiddleware.js";
 const router = express.Router();
 
 router.post("/create", userVerification , Createunit);
-router.post("/delete", userVerification , RemoveUnit);
+router.delete("/delete", userVerification , RemoveUnit);
 router.get("/get", getUnits);
 
 export default router;
